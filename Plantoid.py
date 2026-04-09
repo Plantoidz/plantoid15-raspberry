d588200480bfd53bdbe4856029f737dd59a0484c
yaoe <pdefilippi@gmail.com>
2026-04-09 17:36:45 +0200

    making small changes for Plantoid14 compatibility

--- Plantoid.py
+++ Plantoid.py
@@ -234,7 +234,7 @@ def main():
     if use_serial:
         PORT = os.environ.get('SERIAL_PORT_OUTPUT')
         print("PORT ==== ", PORT)
-        ser = serial_utils.setup_serial(PORT=PORT, baudrate = (plantoid_number == "14" ? 9600 : 115200) )
+        ser = serial_utils.setup_serial(PORT=PORT, baudrate = 9600 if plantoid_number == "14" else 115200) )
         serial_utils.wait_for_arduino(ser)
         serial_utils.send_to_arduino(ser, "awake")  
 
