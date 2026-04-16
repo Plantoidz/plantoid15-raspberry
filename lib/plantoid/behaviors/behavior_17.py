09ee7894da6aceb961762fb4fba66bb3f881fe73
yaoe <pdefilippi@gmail.com>
2026-04-16 21:49:30 +0200

    checking 14 and 17

--- lib/plantoid/behaviors/behavior_17.py
+++ lib/plantoid/behaviors/behavior_17.py
@@ -3,15 +3,15 @@ from lib.plantoid.behaviors import behavior_library as behaviors
 
 def ingurgitate_crypto(plantoid, network, tID, amount):
 
-    question = "What is the future you're dreaming of?"
+    question = "How do you envision the relationship between humans, nature, and machines?"
 
     behaviors.poem_generation(plantoid, network, tID, amount, question)
 
 
 def create_seed_metadata(plantoid, network, tID):
 
-    description = "Plantoid #14 - Seed #" + tID
-    image = "https://ipfs.io/ipfs/QmRcrcn4X6QfSwFnJQ1dNHn8YgW7pbmm6BjZn7t8FW7WFV"
+    description = "Plantoid #17 - Seed #" + tID
+    image = "https://ipfs.io/ipfs/bafybeihkjh6s7ofaxb2nzjcwod3hs7qvubfftixwu35m35z5ijug25wwx4"
 
 
     behaviors.poem_metadata(plantoid, network, tID, description, image)
@@ -21,7 +21,6 @@ def create_seed_metadata(plantoid, network, tID):
 
 
 
-    #
 
 
 
