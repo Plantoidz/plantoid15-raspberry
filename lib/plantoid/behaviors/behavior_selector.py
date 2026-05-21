9c585f37b9fcfa3752a1f099ef8ff013835c6166
yaoe <pdefilippi@gmail.com>
2026-05-21 15:27:22 +0200

    minor bug fix

--- lib/plantoid/behaviors/behavior_selector.py
+++ lib/plantoid/behaviors/behavior_selector.py
@@ -1,3 +1,5 @@
+
+import lib.plantoid.behaviors.behavior_19 as behavior_19
 import lib.plantoid.behaviors.behavior_18 as behavior_18
 import lib.plantoid.behaviors.behavior_17 as behavior_17
 import lib.plantoid.behaviors.behavior_16 as behavior_16
@@ -9,7 +11,13 @@ def get_plantoid_function(plantoid_number, fn_name):
     # TODO: add cases here
 
     print("PLANTOID NUMBER == ", plantoid_number)
-    
+
+    if plantoid_number == 19:
+        fn_dict = {
+            'create_seed_metadata': behavior_19.create_seed_metadata,
+            'ingurgitate_crypto': behavior_19.ingurgitate_crypto
+        }
+
     if plantoid_number == 18:
         fn_dict = {
             'create_seed_metadata': behavior_18.create_seed_metadata,
