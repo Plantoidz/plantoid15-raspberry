e18680edfdf196f64203789881d895086fc8a114
yaoe <pdefilippi@gmail.com>
2026-05-22 21:45:12 +0200

    trying to get printer to work on P19

--- lib/plantoid/pin_utils.py
+++ lib/plantoid/pin_utils.py
@@ -64,7 +64,7 @@ def print_thermal_txt(textual, timeout=10):
             # p = Usb(0x0416, 0x5011, in_ep=0x81, out_ep=0x03)
             # p = Usb(0x0416, 0x5011)
             p = File("/dev/usb/lp0")
-            p.text(textual)
+            p._raw(textual.encode("ascii", "replace") +  + b"\n\n\n\n")
             p.cut()
         except Exception as e:
             result["err"] = e
